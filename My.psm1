dc0098c6cd3476a17688dde8e4980c6abb6c12d6
Anna Vasileva <agvasileva@rt-dc.ru>
2023-07-17 23:03:48 +0300

    initial commit

---
+++ My.psm1
@@ -0,0 +1,50 @@
+﻿
+function New-RandomPassword{
+	<#
+	.SYNOPSIS
+	generate random password
+	.DESCRIPTION
+	Default password length is 14
+	.PARAMETER Length
+	Desired length of the password
+	.EXAMPLE
+	.\New-RandomPassword.ps1 -Length 12
+	#>
+
+	param
+	(
+		[byte]$Length=14
+	)
+	Add-Type -AssemblyName System.Web
+	[System.Web.Security.Membership]::GeneratePassword($Length,2)
+}
+
+function Convert-ToBase64{
+	<#
+	.SYNOPSIS
+	convert text to base64 encoding
+	.DESCRIPTION
+	only first string in parameters
+	.PARAMETER text
+	text to encode
+	.EXAMPLE
+	.\COnvert-tobase64 -text password
+	#>
+    param
+    (
+        [string]$text=""
+    )
+    
+    [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($text))
+}
+
+function Convert-FromBase64toUTF8{
+    param(
+        [string]$text=""
+    )
+
+    [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($text))
+}
+
+New-Alias -Name tobase64 -Value Convert-toBase64
+New-Alias -Name frombase64 -Value Convert-fromBase64toUTF8
